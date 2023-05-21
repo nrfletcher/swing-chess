428c5e96aa5b1bfaa67149c204d82d5f34d3ee5f
Nesta Fletcher <fletchernesta@gmail.com>
2023-05-21 16:55:27 -0400

    refactored file hierarchy

---
+++ src/main/Tester/Game.java
@@ -0,0 +1,10 @@
+package Tester;
+
+/*  Game class is the origin for a new game
+ *  When launched, all values and piece positions are set to game begin
+ *  @author nrileyfletcher
+ *  @brief  Entry to our chess program
+ */
+
+public class Game {
+}
